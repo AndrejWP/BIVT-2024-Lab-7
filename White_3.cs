a5dfb1fe09a44f3c76219703972af1889079c290
AndrejWP <m2403371@edu.misis.ru>
2025-04-12 12:33:35 +0300

    Update White_3.cs

--- White_3.cs
+++ White_3.cs
@@ -1,4 +1,3 @@
-﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,43 +11,26 @@ namespace Lab_7
     {
         public class Student
         {
-            private string _name;
-            private string _surname;
-            private int[] _marks;
-            private int _skipped;
-            public string Surname
-            {
-                get
-                {
-                    if (_surname == null) return default;
-                    return _surname;
-                }
-            }
-            public string Name
-            {
-                get
-                {
-                    if (_name == null) return default;
-                    return _name;
-                }
-            }
-            public double AvgMark
-            {
-                get
-                {
-                    if (_marks == null || _marks.Length == 0)
-                    {
-                        return 0;
-                    }
-                    return (double)_marks.Sum() / _marks.Length;
-                }
-            }
+            protected string _name;
+            protected string _surname;
+            protected int[] _marks;
+            protected int _skipped;
+
+            public string Surname => _surname ?? default;
+            public string Name => _name ?? default;
+
+            public double AvgMark =>
+                (_marks == null || _marks.Length == 0)
+                ? 0
+                : (double)_marks.Sum() / _marks.Length;
+
             public int Skipped => _skipped;
+
             public Student(string name, string surname)
             {
                 _name = name;
                 _surname = surname;
-                _marks = new int[0];
+                _marks = Array.Empty<int>();
                 _skipped = 0;
             }
 
@@ -60,88 +42,70 @@ namespace Lab_7
                 _skipped = student._skipped;
             }
 
-
-            public class Undergraduate : Student
-            {
-                
-                public Undergraduate(string name, string surname) : base(name, surname)
-                {
-                }
-
-                
-                public Undergraduate(Student student) : base(student)
-                {
-                }
-
-                
-                public void WorkOff(int mark)
-                {
-                    if (_skipped > 0)
-                    {
-                        _skipped--;
-                        Lesson(mark);
-                    }
-                    else
-                    {
-                        
-                        int index = Array.IndexOf(_marks, 2);
-                        if (index != -1)
-                        {
-                            _marks[index] = mark;
-                        }
-                    }
-                }
-
-
-                public new void Print()
-                {
-                    Console.WriteLine($"Студент: {Name} {Surname}, Средняя оценка: {AvgMark:F2}, Пропуски: {Skipped}");
-                }
-            }
             public void Lesson(int mark)
             {
-                if (mark == 0)
-                {
-                    _skipped++;
-                }
-                else
-                {
-                    if (_marks == null) return;
-                    int[] new_marks = new int[_marks.Length + 1];
-                    for (int i = 0; i < _marks.Length; i++)
-                    {
-                        new_marks[i] = _marks[i];
-                    }
-
-                    new_marks[new_marks.Length - 1] = mark;
-                    _marks = new_marks;
-                }
+                if (mark == 0) _skipped++;
+                else _marks = [.. _marks, mark];
             }
+
             public static void SortBySkipped(Student[] array)
             {
                 if (array == null || array.Length == 0) return;
-                int n = array.Length;
-                bool y;
-                for (int i = 0; i < n - 1; i++)
+
+                for (int i = 0; i < array.Length - 1; i++)
                 {
-                    y = false;
-                    for (int j = 0; j < n - 1 - i; j++)
+                    bool swapped = false;
+                    for (int j = 0; j < array.Length - 1 - i; j++)
                     {
-                        if (array[j].Skipped < array[j + 1].Skipped)
-                        {
-                            (array[j], array[j + 1]) = (array[j + 1], array[j]);
-                            y = true;
-                        }
+                        if (array[j].Skipped >= array[j + 1].Skipped) continue;
+
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                        swapped = true;
                     }
-                    if (!y) break;
+                    if (!swapped) break;
+                }
+            }
+
+            public virtual void Print()
+            {
+                Console.WriteLine(
+                    $"Имя: {_name}, " +
+                    $"Фамилия: {_surname}, " +
+                    $"Средняя оценка: {AvgMark:F2}, " +
+                    $"Пропуски: {Skipped}"
+                );
+            }
+        }
+
+        public class Undergraduate : Student
+        {
+            public Undergraduate(string name, string surname) : base(name, surname) { }
+
+            public Undergraduate(Student student) : base(student) { }
+
+            public void WorkOff(int mark)
+            {
+                if (_skipped > 0)
+                {
+                    _skipped--;
+                    Lesson(mark);
+                    return;
                 }
 
-            }
-            public void Print()
-            {
-                Console.WriteLine($"Имя: {_name}, Фамилия: {_surname}, Средняя оценка: {AvgMark:F2}, Пропуски: {_skipped}");
+                int index = Array.IndexOf(_marks, 2);
+                if (index != -1) _marks[index] = mark;
             }
 
+            public override void Print()
+            {
+                Console.WriteLine(
+                    $"Студент: {Name} {Surname}, " +
+                    $"Средняя оценка: {AvgMark:F2}, " +
+                    $"Пропуски: {Skipped}"
+                );
+            }
         }
     }
+
+
 }
